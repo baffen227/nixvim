0f9208940f48f4fb9d9ba981b9468ce5238fb94d
Harry Chen <baffen227@gmail.com>
2024-09-06 11:23:32 +0800

    Add options for autocomplete

--- config/options.nix
+++ config/options.nix
@@ -16,5 +16,17 @@
 
     ## Do not wrap text
     wrap = false;
-  };
+
+
+    # Autocomplete (when the user presses the key combination <Ctrl+N>)
+
+    ## Only use current buffer for autocomplete
+    complete = ".";
+
+    ## Show menu even with one entry only, do not select nor insert, and show preview
+    completeopt = [ "menuone" "noselect" "noinsert" "preview" ];
+
+    ## Menu height (number of lines)
+    pumheight = 10;
+   };
 }
