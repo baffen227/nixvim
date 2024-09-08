bcffb39710c38ef51db48ad871d2d3127dfce9d4
Harry Chen <baffen227@gmail.com>
2024-09-08 22:44:05 +0800

    Add a keymap to jump to definition in help window

--- config/functions.nix
+++ config/functions.nix
@@ -1,2 +1,14 @@
 {
+  autoCmd = [
+    # Go to tag in help window
+    {
+      event = "FileType";
+      pattern = "help";
+      callback = { __raw = ''
+        function (opts)
+	  vim.keymap.set('n', '<Enter>', '<C-]>')
+	end'';
+      };
+    }
+  ];
 }
