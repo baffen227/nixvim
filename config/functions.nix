8ac09ba01b03783f60f4af60a15e29e7e9fbc0c4
Harry Chen <baffen227@gmail.com>
2024-09-08 22:59:08 +0800

    Add autocmd for autocompletion

--- config/functions.nix
+++ config/functions.nix
@@ -1,5 +1,6 @@
 {
   autoCmd = [
+    # HELP
     # Go to tag in help window
     {
       event = "FileType";
@@ -7,6 +8,21 @@
       callback = { __raw = ''
         function (opts)
 	  vim.keymap.set('n', '<Enter>', '<C-]>')
+	end'';
+      };
+    }
+
+    # AUTOCOMPLETE
+    # Every time we type a character, we will see the pop-up menu
+    # with the autocomplete options provided by Neovim.
+    {
+      event = "InsertCharPre";
+      callback = { __raw = ''
+        function (opts)
+	  if vim.fn.pumvisible() == 0 then
+	    local key = vim.api.nvim_replace_termcodes('<C-n>', true, false, true)
+	    vim.api.nvim_feedkeys(key, 'n', false)
+	  end
 	end'';
       };
     }
