eda03fcd8eea147213df04ce96b52b744c4591a9
Harry Chen <baffen227@gmail.com>
2024-09-13 16:34:30 +0800

    Revise gitsigns keymaps

--- config/plugins/git/gitsigns.nix
+++ config/plugins/git/gitsigns.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       trouble = true;
-      current_line_blame = true;
+      current_line_blame = false;
       current_line_blame_formatter = "   <author>, <committer_time:%R> • <summary>";
       signs = {
         add = {
@@ -30,7 +30,70 @@
   keymaps = [
     {
       mode = "n";
-      key = "<leader>ghd";
+      key = "<S-PageDown>";
+      action = ":Gitsigns next_hunk<CR>";
+      options = {
+        silent = true;
+        desc = "Next Hunk";
+      };
+    }
+    {
+      mode = "n";
+      key = "<S-PageUp>";
+      action = ":Gitsigns prev_hunk<CR>";
+      options = {
+        silent = true;
+        desc = "Previous Hunk";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>hs";
+      action = ":Gitsigns stage_hunk<CR>";
+      options = {
+        silent = true;
+        desc = "Stage Hunk";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>hS";
+      action = ":Gitsigns stage_buffer<CR>";
+      options = {
+        silent = true;
+        desc = "Stage Buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>hu";
+      action = ":Gitsigns undo_stage_hunk<CR>";
+      options = {
+        silent = true;
+        desc = "Undo Stage Hunk";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>hr";
+      action = ":Gitsigns reset_hunk<CR>";
+      options = {
+        silent = true;
+        desc = "Reset Hunk";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>hR";
+      action = ":Gitsigns reset_buffer<CR>";
+      options = {
+        silent = true;
+        desc = "Reset Buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>hd";
       action = ":Gitsigns diffthis<CR>";
       options = {
         silent = true;
